a17e3ea48be0e63dc0c1068987d3b44f793d9797
RUDRANAVYA05 <navyasln555@gmail.com>
2021-08-07 12:26:49 +0530

    initial code set

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const [tabs, tabsPanels] = [
+	Array.from(document.querySelectorAll(".tabs li")),
+	Array.from(document.querySelectorAll(".tabs-panel"))
+];
+
+tabs.forEach((tab) => {
+	tab.addEventListener("click", () => {
+		const target = document.querySelector(`#${tab.dataset.target}`);
+		removeActiveClass([tabs, tabsPanels]);
+		tab.classList.add("active");
+		target.classList.add("active");
+	});
+});
+
+const removeActiveClass = (el) => {
+	el.forEach((item) => {
+		item.find((e) => e.classList.contains("active")).classList.remove("active");
+	});
+};
